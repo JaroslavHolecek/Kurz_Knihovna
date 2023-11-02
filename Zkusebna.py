e59c5280456a15c86143408a380b4c27cf306c83
Jaroslav Holeček <holecek.jaroslav@email.cz>
2023-11-02 18:26:09 +0100

    Add files via upload

--- Zkusebna.py
+++ Zkusebna.py
@@ -1,14 +1,25 @@
-pocet_sloupecku = 4
-pocet_radku = 6
-vysledky = []
-for i in range(pocet_radku):
-    vysledky.append([0]*pocet_sloupecku)
+class Auto:
+    def __init__(self):
+        self.barva = "cervena"
+        self.pozice = 0
+        self.rychlost = 10
+
+    def __str__(self):
+        return f"p: {self.pozice} r: {self.rychlost} {self.barva}"
+    def popojed(self):
+        self.pozice = self.pozice + self.rychlost
+
+trabant = Auto()
+skoda = Auto()
+print(skoda)
+print(trabant)
+trabant.popojed()
+print(skoda)
+print(trabant)
 
 
-print(vysledky)
-vysledky[3][2] = 55
-print(vysledky)
 
-for radek in range(2, 7):
-    for sloupec in range(2, 6):
-        print(f"r:{radek} s{sloupec}")
+
+# for radek in range(2, 7):
+#     for sloupec in range(2, 6):
+#         print(f"r:{radek} s{sloupec}")
